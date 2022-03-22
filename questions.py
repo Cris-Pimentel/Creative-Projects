67dc3832124e4292e7125f5a81e3c3c4d7aa6055
Cris-Pimentel <78329974+Cris-Pimentel@users.noreply.github.com>
2022-03-21 19:10:34 -0700

    Add files via upload

---
+++ questions.py
@@ -0,0 +1,26 @@
+quiz = {
+    1 : {
+        "question" : "Who is the creator of bitcoin?",
+        "answer" : "Satoshi Nakamoto"
+    },
+    2 : {
+        "question" : "Who is the creator of Ethereum?",
+        "answer" : "Vitalik Buterin"
+    },
+    3 : {
+        "question" : "What hashing function is used in orde to validate and confirm bitcoin transactions?",
+        "answer" : "Proof of work"
+    },
+    4 : {
+        "question" : "What algortihmic function is used to validate transactions using ethereum?",
+        "answer" : "Proof of stake"
+    },
+    5 : {
+        "question" : "What year was the bitcoin whitepaper first published?",
+        "answer" : "2008"
+    },
+    6 : {
+        "question" : "What does defi stand for?",
+        "answer" : "Decentralized finance"
+    }
+}
